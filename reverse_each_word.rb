f8b37650aa5db7e05869801429436d9a44cf3587
Matt Maher <mattmaher0@gmail.com>
2018-05-05 15:42:16 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,14 @@
 def reverse_each_word(string)
-  string.split.collect |word|
-  word.reverse + " "
-end  
-
-def reverse_each_word(string)
   array = ""
   string.split.each do |word|
     array << word.reverse + " "
   end
   array.strip
 end
+  
+def reverse_each_word(string)
+  string.split.collect |word|
+  word.reverse + " "
+end  
+
   
